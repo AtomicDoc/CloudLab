277c43ddfba861d341e60faeebf8244b4b3b56f8
Desmond Cheeseboro <112589623+AtomicDoc@users.noreply.github.com>
2026-03-18 18:24:40 -0400

    Add service to install Docker using a script

--- profile.py
+++ profile.py
@@ -11,6 +11,11 @@ node.routable_control_ip = "true"
 node.addService(rspec.Execute(shell="/bin/sh", command="sudo apt update"))
 node.addService(rspec.Execute(shell="/bin/sh", command="sudo apt install -y apache2"))
 node.addService(rspec.Execute(shell="/bin/sh", command='sudo systemctl status apache2'))
+node.addService(rspec.Execute(
+    shell="bash",
+    command="/local/repository/install-docker.sh"
+))
+
 
 # Print the RSpec to the enclosing page.
 portal.context.printRequestRSpec()
