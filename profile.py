f26f4b530551a3cfbc6f7814fbb3cc7270cc06aa
Desmond Cheeseboro <112589623+AtomicDoc@users.noreply.github.com>
2026-03-29 03:41:45 -0400

    Replace service execution with pg.Execute for startup.sh

--- profile.py
+++ profile.py
@@ -11,10 +11,7 @@ node.routable_control_ip = "true"
 node.addService(rspec.Execute(shell="/bin/sh", command="sudo apt update"))
 node.addService(rspec.Execute(shell="/bin/sh", command="sudo apt install -y apache2"))
 node.addService(rspec.Execute(shell="/bin/sh", command='sudo systemctl status apache2'))
-node.addService(rspec.Execute(
-    shell="bash",
-    command="/local/repository/CloudLab/startup.sh"
-))
+node.addService(pg.Execute(shell="bash", command="cd /local/repository/cloudlab && ./startup.sh"))
 
 
 # Print the RSpec to the enclosing page.
