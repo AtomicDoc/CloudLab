9ce5de5f0066e95390c27b0e0431561d8875986a
Desmond Cheeseboro <112589623+AtomicDoc@users.noreply.github.com>
2026-03-29 04:39:30 -0400

    added bash to startup

--- profile.py
+++ profile.py
@@ -11,7 +11,7 @@ node.routable_control_ip = "true"
 node.addService(rspec.Execute(shell="/bin/sh", command="sudo apt update"))
 node.addService(rspec.Execute(shell="/bin/sh", command="sudo apt install -y apache2"))
 node.addService(rspec.Execute(shell="/bin/sh", command='sudo systemctl status apache2'))
-node.addService(rspec.Execute(shell="bash", command="cd /local/repository/cloudlab && ./startup.sh"))
+node.addService(rspec.Execute(shell="bash", command="cd /local/repository/cloudlab && bash ./startup.sh"))
 
 
 # Print the RSpec to the enclosing page.
