b8a2e6120d2d97b6e7afb7a55d89ad419399257d
Desmond Cheeseboro <112589623+AtomicDoc@users.noreply.github.com>
2026-03-29 04:10:26 -0400

    Updated statup script

--- cloudlab/startup.sh
+++ cloudlab/startup.sh
@@ -1,17 +1,26 @@
 #!/bin/bash
 set -e
 
+# Install Docker
 curl -fsSL https://get.docker.com | sh
 
-# start Docker
+# Start Docker
 sudo systemctl enable docker
 sudo systemctl start docker
 
+# Allow current user to run docker
 sudo usermod -aG docker $USER
 
 sleep 5
 
 cd /local/repository
 
+# Dynamic BASE_URL for this CloudLab node
 HOST=$(hostname -f)
 echo "BASE_URL=http://$HOST:8080" > .env
+
+# Pull images from registry 
+docker compose pull
+
+# Start the stack using pulled images 
+docker compose up -d
